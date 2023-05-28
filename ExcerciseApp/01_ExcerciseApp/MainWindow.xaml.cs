efab9ff282e205c25199178192223ef81d12300c
kimjihyeon-angela <angela9830@naver.com>
2023-05-29 01:16:14 +0900

    2023.05.29

---
+++ ExcerciseApp/01_ExcerciseApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _01_ExcerciseApp.Views;
+using ControlzEx.Theming;
+
+namespace _01_ExcerciseApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncWithAppMode;
+            ThemeManager.Current.SyncTheme();
+        }
+
+        private void MnuWeight_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            //ActiveItem.Content = new Views.MainPage();
+            // Frame => Page.xaml
+            // ContentControl => UserControl.xaml사용해야함
+
+            // ActiveItem.Content = new Views.DataBaseControl();
+        }
+    }
+}
